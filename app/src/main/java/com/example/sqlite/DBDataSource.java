11aa1cc3cdfe648ac594063033cda8bf194cfc02
septakusumaningtyas <septakusumaningtyas@gmail.com>
2019-11-14 13:08:25 +0700

    Edit Data

--- app/src/main/java/com/example/sqlite/DBDataSource.java
+++ app/src/main/java/com/example/sqlite/DBDataSource.java
@@ -96,4 +96,35 @@ public class DBDataSource {
         cursor.close();
         return daftarBarang;
     }
+
+    //ambil satu barang sesuai id
+    public Barang getBarang(long id)
+    {
+        Barang barang = new Barang(); //inisialisasi barang
+        //select query
+        Cursor cursor = database.query(DBHelper.TABLE_NAME, allColumns, "_id ="+id, null, null, null, null);
+        //ambil data yang pertama
+        cursor.moveToFirst();
+        //masukkan data cursor ke objek barang
+        barang = cursorToBarang(cursor);
+        //tutup sambungan
+        cursor.close();
+        //return barang
+        return barang;
+    }
+
+    //update barang yang diedit
+    public void updateBarang(Barang b)
+    {
+        //ambil id barang
+        String strFilter = "_id=" + b.getId();
+        //memasukkan ke content values
+        ContentValues args = new ContentValues();
+        //masukkan data sesuai dengan kolom pada database
+        args.put(DBHelper.COLUMN_NAME, b.getNama_barang());
+        args.put(DBHelper.COLUMN_MERK, b.getMerk_barang());
+        args.put(DBHelper.COLUMN_HARGA, b.getHarga_barang() );
+        //update query
+        database.update(DBHelper.TABLE_NAME, args, strFilter, null);
+    }
 }
